268d4b35fd7ef5de1411cd622f26eec47645c09e
Salma Maghfira <82102262+omnom0-0@users.noreply.github.com>
2025-06-11 11:33:49 +0700

    Add src/app/dashboard/page.tsx 1749616429886

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Dashboard from "@/components/Dashboard";
+
+const DashboardPage = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default DashboardPage;
